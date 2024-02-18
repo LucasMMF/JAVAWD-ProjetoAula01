88a4b38e315fecce057e2ffb9ae895a21e469b80
LucasMMF <mateusmidao@gmail.com>
2024-02-18 20:46:35 -0300

    feat!: initial program
    coding the initial program for the lecture,
    this program is a simple println in the console,
    showing basic information to get things started.

---
+++ src/principal/Program.java
@@ -0,0 +1,16 @@
+// Identificação do pacote
+package principal;
+
+// Definição da classe
+public class Program {
+
+	// Método padrão para execução do projeto/classe
+	public static void main(String[] args) {
+
+		// Imprimindo mensagem na console do eclipse...
+		// syso -> CTRL + espaço -> atalho pro System.out.println no eclipse.
+		System.out.println("Aula 01 - Java Webdeveloper COTI Informática");
+
+	}
+
+}
